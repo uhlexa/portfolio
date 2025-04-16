d5968e3e92c64e6a3002eb687e63bf145557a29c
uhlexa <afernandeztobias@ucsd.edu>
2025-04-16 13:44:00 -0700

    new tab

--- global.js
+++ global.js
@@ -38,5 +38,15 @@ for (let p of pages) {
     a.href = url;
     a.textContent = title;
     nav.append(a);
+
+
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname,
+    );
+
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
 }
 
