cf99af85b7736f22742a56c199b00485b1fe5a04
uhlexa <afernandeztobias@ucsd.edu>
2025-04-16 11:48:15 -0700

    step 2

--- global.js
+++ global.js
@@ -3,3 +3,9 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname,
+  );
+
+  currentLink?.classList.add('current');
