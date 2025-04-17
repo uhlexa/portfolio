b154a1de33b26ac1c2f6320505b87bd60bf759ac
uhlexa <afernandeztobias@ucsd.edu>
2025-04-16 23:31:06 -0700

    typos

--- global.js
+++ global.js
@@ -63,11 +63,10 @@ document.body.insertAdjacentHTML(
       </label>`,
   );
 
-const select = document.querySelector('#color-scheme-select')
+const select = document.querySelector('.color-scheme-select')
 
 select.addEventListener('input', function (event) {
   console.log('color scheme changed to', event.target.value);
-
   document.documentElement.style.setProperty('color-scheme', event.target.value);
 });
 
