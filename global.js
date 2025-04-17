450fc4deab67e0fc7f9c55711c412180bdff64fd
uhlexa <afernandeztobias@ucsd.edu>
2025-04-16 22:03:36 -0700

    css

--- global.js
+++ global.js
@@ -61,3 +61,8 @@ document.body.insertAdjacentHTML(
       </label>`,
   );
 
+const select = document.querySelector('#color-scheme-select')
+
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+});
