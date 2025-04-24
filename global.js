6f7d8c43c17c77d1e7f580981e466f0f0a456f4d
uhlexa <afernandeztobias@ucsd.edu>
2025-04-24 16:03:00 -0700

    sh

--- global.js
+++ global.js
@@ -85,18 +85,19 @@ export async function fetchJSON(url) {
   try {
     // Fetch the JSON file from the given URL
     const response = await fetch(url);
-    console.log(response);
 
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
 
     //const data = await response.json();
- //   return data;
+  //  return data;
 
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
-
-fetchJSON('./lib/projects.json')
+fetchJSON('./lib/projects.json').then((projects) => {
+  console.log('Loaded project data:', projects);
+  // You can then render the projects to the page here
+});
