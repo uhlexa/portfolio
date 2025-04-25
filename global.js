389d444100046d2be910dbc0e0e38b8b649b8ec8
uhlexa <afernandeztobias@ucsd.edu>
2025-04-24 18:11:57 -0700

    dome

--- global.js
+++ global.js
@@ -90,15 +90,35 @@ export async function fetchJSON(url) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
 
-    //const data = await response.json();
-  //  return data;
+    const data = await response.json();
+    return data;
 
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
 
-fetchJSON('../lib/projects.json').then((projects) => {
-  console.log('Loaded project data:', projects);
-  // You can then render the projects to the page here
-});
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  // Your code will go here
+  containerElement.innerHTML = '';
+
+  if (!Array.isArray(project)) {
+    console.error('"projects" not an array');
+    return;
+  }
+
+  if (!(containerElement instanceof Element)) {
+    console.error('"containerElement" not a valid DOM element');
+    return;
+  }
+
+  for (const project of projects) {}
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <h3>${project.title}</h3>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+  containerElement.appendChild(article);
+
+}
