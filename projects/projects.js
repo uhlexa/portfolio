7f08e7095c21936a8deee79a2b71e6747e58b994
uhlexa <afernandeztobias@ucsd.edu>
2025-04-30 22:33:40 -0700

    change

--- projects/projects.js
+++ projects/projects.js
@@ -3,3 +3,42 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+let data = [1, 2];
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc) => {
+    // TODO, fill in step for appending path to svg using D3
+  });
+
+
+let colors = ['gold', 'purple'];
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr(...) // Fill in the attribute for fill color via indexing the colors variable
+})
